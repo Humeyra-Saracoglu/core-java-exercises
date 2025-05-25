d02c3e583bdc5a9d1165353baf7c2c3e186da0e2
Helin Saraçoğlu <helinhumeyrasaracoglu@gmail.com>
2025-05-25 16:06:20 +0300

    Tüm tür dönüşümleri anlatıldı.

---
+++ Kullanıcıdan Deger Alma/TemelVeriTipleri.java
@@ -0,0 +1,23 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class TemelVeriTipleri {
+    public static void main(String[] args) {
+        int yas = 23;
+        double boy = 1.68;
+        float agirlik = 59.0F;
+        char cinsiyet = 'K';
+        boolean ogrenciMi = true;
+        String isim = "Helin Hümeyra";
+        double PI = 3.14159;
+        System.out.println("İsim: " + isim);
+        System.out.println("Yaş: " + yas);
+        System.out.println("Boy: " + boy + " m");
+        System.out.println("Ağırlık: " + agirlik + " kg");
+        System.out.println("Cinsiyet: " + cinsiyet);
+        System.out.println("Öğrenci mi?:  " + ogrenciMi);
+        System.out.println("Pi sayısı (sabit): 3.14159");
+    }
+}
---
+++ Tur Donusumleri/src/Test.java
@@ -0,0 +1,55 @@
+public class Test {
+    public static void main(String[] args) {
+        // TÜR DÖNÜŞÜMLERİ
+
+        /*
+        STRİNG tipinden INT tipine dönüştürme
+        INT tipinden STRİNG tipine dönüştürme
+        DOUBLE tipinden INT tipine dönüştürme
+        INT tipinden DOUBLE tipine dönüştürme
+        FLOAT tipinden LONG tipine dönüştürme
+        LONG tipinden FLOAT tipine dönüştürme
+        LONG tipinden STRİNG tipine dönüştürme
+        FLOAT tipinden STRİNG tipine dönüştürme
+        BYTE tipinden STRİNG tipine dönüştürme
+         */
+
+//      String a = "5";
+//      int b = Integer.valueOf(a);
+//      int b = Integer.parseInt(a);
+
+//      int a = 21;
+//      String b = String.valueOf(a);
+
+//      double a = 5.50;
+//      int b = (int)a;
+//      System.out.println(b);
+
+
+//      int a = 5;
+//      double b = (double)a;
+//      System.out.println(b);
+
+//      float a = 5.12f;
+//      long b = (long)a;
+//      System.out.println(b);
+
+//      long a = 5;
+//      float b = (float)a;
+//      System.out.println(b);
+
+//      long a = 5;
+//      String b = String.valueOf(a);
+//      System.out.println(b+2);
+
+//      float a = 5.12f;
+//      String b = String.valueOf(a);
+//      System.out.println(b+ 1.2f);
+
+        byte a = 55;
+        String b = Byte.toString(a);
+        System.out.println(b+2);
+
+
+    }
+}
