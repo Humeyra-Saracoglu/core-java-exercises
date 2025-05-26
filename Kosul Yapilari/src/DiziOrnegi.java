190ecf6201154855db682b25844b9dcea6a7635e
Helin Hümeyra Saraçoğlu <helinhumeyrasaracoglu@gmail.com>
2025-05-26 19:18:07 +0300

    Update DiziOrnegi.java

--- Kosul Yapilari/src/DiziOrnegi.java
+++ Kosul Yapilari/src/DiziOrnegi.java
@@ -1,11 +1,10 @@
 public class DiziOrnegi {
     public static void main(String[] args) {
-        // Amaç: Dizilerin temel yapısını ve elemanlara erişimi öğrenmek
 
-        // 5 elemanlı bir int dizisi oluştur
+
         int[] sayilar = {10, 20, 30, 40, 50};
 
-        // for döngüsü ile dizinin elemanlarını yazdır
+
         for (int i = 0; i < sayilar.length; i++) {
             System.out.println("Dizi[" + i + "] = " + sayilar[i]);
         }
