f71e317e080defeac5582053aa073dab15beb78d
Helin Saraçoğlu <helinhumeyrasaracoglu@gmail.com>
2025-05-28 10:54:13 +0300

    [JA-3] - Receiving data from the user

---
+++ BEBKA_JAVA_Temelleri/src/KullanıcıdanDegerAlma.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class KullanıcıdanDegerAlma {
+    public static void main(String[] args) {
+
+// scanner metodunu kullanıyoruz
+        /*
+        nextLine()    -----> String
+        nextInt()     -----> Integer
+        nextByte()    -----> Byte
+        nextFloat()   -----> Float
+        nextDouble()  -----> Double
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        /*
+        System.out.println("Adınızı Giriniz:");
+
+        String isim = scanner.nextLine();
+
+        System.out.println("Soyadınızı Giriniz: ");
+
+        String soyisim = scanner.nextLine();
+
+        System.out.println("Adınız ve Soyadınız:  " + isim + " " + soyisim);
+
+        */
+        /*
+        System.out.println("Yaşınızı Giriniz: ");
+        int yas = scanner.nextInt();
+        System.out.println("Yaşınız: " + yas);
+         */
+        System.out.print("Kilonuzu Giriniz: ");
+        double kilo = scanner.nextDouble();
+
+        System.out.print("Kilonuz: " + kilo);
+
+
+            }
+        }
