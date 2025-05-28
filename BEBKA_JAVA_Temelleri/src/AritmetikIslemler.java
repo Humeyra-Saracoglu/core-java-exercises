f378a3d01e30eb7922a3ebddc4b64a251f6fc6be
Helin Saraçoğlu <helinhumeyrasaracoglu@gmail.com>
2025-05-28 10:55:54 +0300

    [JA-4] - Arithmetic Operations

--- BEBKA_JAVA_Temelleri/src/AritmetikIslemler.java
+++ BEBKA_JAVA_Temelleri/src/AritmetikIslemler.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DortIslem {
+public class AritmetikIslemler {
     public static void main(String[] args) {
         // Amaç: Toplama, çıkarma, çarpma ve bölme işlemlerini yapmayı öğrenmek
 
