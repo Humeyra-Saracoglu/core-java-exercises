7d9a8d5a513e9dd0a7fd323ad8c0da5a68369dc7
Helin Saraçoğlu <helinhumeyrasaracoglu@gmail.com>
2025-05-30 12:51:28 +0300

    [JA-8] -  Use of Static

---
+++ BEBKA_JAVADA_OOP/src/Matematik.java
@@ -0,0 +1,13 @@
+public class Matematik {
+
+    public static int carp(int sayi1, int sayi2)
+    {
+        return sayi1 * sayi2;
+    }
+
+    public static void main(String[] args)
+    {
+        int sonuc = Matematik.carp(23, 87);
+        System.out.println("Sonuç : " + sonuc);
+    }
+}
