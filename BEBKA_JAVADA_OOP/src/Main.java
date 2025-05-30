dbaf51e226fe112790e60d484009d0d66ad8e6c7
Helin Saraçoğlu <helinhumeyrasaracoglu@gmail.com>
2025-05-30 12:55:40 +0300

    [JA-] -  Use of Polymorphism

---
+++ BEBKA_JAVADA_OOP/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal hayvan1 = new Cat();
+        Animal hayvan2 = new Dog();
+
+        hayvan1.makeSound();
+        hayvan2.makeSound();
+    }
+}
+
+class Animal {
+    public void makeSound() {
+        System.out.println("Bir hayvan ses çıkarıyor...");
+    }
+}
+
+class Cat extends Animal {
+    public void makeSound() {
+        System.out.println("Kedi: Miyav!");
+    }
+}
+
+class Dog extends Animal {
+    public void makeSound() {
+        System.out.println("Köpek: Hav hav!");
+    }
+}
