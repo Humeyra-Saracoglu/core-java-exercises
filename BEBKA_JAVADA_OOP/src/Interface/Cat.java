94e1e428f3b8b5f2ecb2cd5fb56dfd0fcfca5650
Helin Saraçoğlu <helinhumeyrasaracoglu@gmail.com>
2025-06-01 23:38:46 +0300

    [JA-11] -  Use of Interface

---
+++ BEBKA_JAVADA_OOP/src/Interface/Cat.java
@@ -0,0 +1,10 @@
+package Interface;
+
+public class Cat implements Animal
+{
+    public void sound()
+    {
+        System.out.println("Miyauvvv");
+    }
+}
+
---
+++ BEBKA_JAVADA_OOP/src/Interface/Main.java
@@ -0,0 +1,14 @@
+package Interface;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        dog.sound();
+        cat.sound();
+    }
+}
+
