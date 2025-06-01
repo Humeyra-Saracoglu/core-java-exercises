5784d3b3658dc6c27cd2c7c5a47ad999dff102f6
Helin Saraçoğlu <helinhumeyrasaracoglu@gmail.com>
2025-06-01 23:30:40 +0300

    [JA-7] -  Use of Method Overloading

---
+++ BEBKA_JAVADA_OOP/src/MethodOverloading/Main.java
@@ -0,0 +1,14 @@
+package MethodOverloading;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        int toplam1 = calc.add(28, 30);
+        int toplam2 = calc.add(34, 41, 88);
+
+        System.out.println("2 sayının toplamı : " + toplam1);
+        System.out.println("3 sayının toplamı : " + toplam2);
+    }
+}
+
