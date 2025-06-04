dcac500580562dda2a9b63820b53f850bb3549fc
Helin Saraçoğlu <helinhumeyrasaracoglu@gmail.com>
2025-06-04 17:32:59 +0300

    [JA-1] -  Enum - Define Constants

---
+++ BEBKA_JAVA_UYGULAMA/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.example.enums.Gunler;
+/*
+--------[JA-1] ENUM- Define Constants------------
+
+public class Main {
+    public static void main(String[] args) {
+        Gunler bugun = Gunler.CARSAMBA;
+
+        System.out.println("Bugün günlerden: " + bugun);
+    }
+}
+*/
+
---
+++ BEBKA_JAVA_UYGULAMA/src/main/java/org/example/enums/Gunler.java
@@ -0,0 +1,12 @@
+package org.example.enums;
+
+public enum Gunler {
+        PAZARTESI,
+        SALI,
+        CARSAMBA,
+        PERSEMBE,
+        CUMA,
+        CUMARTESI,
+        PAZAR
+    }
+
