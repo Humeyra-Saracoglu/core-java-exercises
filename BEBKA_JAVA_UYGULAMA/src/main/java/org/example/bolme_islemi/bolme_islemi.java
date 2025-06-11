b74f0535d01cdf990c061e1c69ccb1ca7b4f20df
Helin Saraçoğlu <helinhumeyrasaracoglu@gmail.com>
2025-06-11 12:09:12 +0300

    Exception Handling – Try-Catch

---
+++ BEBKA_JAVA_UYGULAMA/src/main/java/org/example/bolme_islemi/bolme_islemi.java
@@ -0,0 +1,27 @@
+package org.example.bolme_islemi;
+
+import java.util.Scanner;
+
+public class bolme_islemi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+
+            System.out.print("Birinci sayıyı girin: ");
+            int sayi1 = scanner.nextInt();
+
+            System.out.print("İkinci sayıyı girin: ");
+            int sayi2 = scanner.nextInt();
+
+            int sonuc = sayi1 / sayi2;
+            System.out.println("Sonuç: " + sonuc);
+        }
+
+        catch (ArithmeticException e)
+        {
+            System.out.println("Hata: Bir sayı sıfıra bölünemez!");
+        }
+
+    }
+}
